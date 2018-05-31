13beba7e1bb6faf944838f66070bc496b60bca10
Margaret Blauvelt <margaret@blauvelt.com>
2018-05-30 23:15:10 -0700

    Add static header and footer components

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <footer role="contentinfo">
+          <summar>&copy;2018 Margaret Blauvelt</summar>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
