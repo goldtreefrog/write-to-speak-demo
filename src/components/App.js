91a8d3035d0c6a54060cdc8babad348c11b9f9dd
Margaret Blauvelt <margaret@blauvelt.com>
2018-06-02 18:12:58 -0700

    Add a 404 (not found) page, fix header.js display, start adding html for write.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/App.css";
 import Header from "./header";
 import Footer from "./footer";
 import Write from "./write";
 import Home from "./home";
+import NotFound from "./not-found";
+
+const routes = (
+  <Router>
+    <div className="App">
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/write" component={Write} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+      <Footer />
+    </div>
+  </Router>
+);
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <main>
-            <Route path="/" component={Home} exact />
-            <Route path="/write" component={Write} />
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    );
+    return routes;
   }
 }
 
---
+++ src/components/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Uh-oh!</h1>
+      <h2>We cannot find the Web page you have requested.</h2>
+      <p>Please try one of the links above.</p>
+    </div>
+  );
+}
+export default NotFound;
