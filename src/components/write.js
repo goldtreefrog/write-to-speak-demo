4594d430169f4447df53d7696661533f6a361374
Margaret Blauvelt <margaret@blauvelt.com>
2018-06-03 11:34:19 -0700

    Remove accesskeys in write (because I will handle differently eventually anyway)

--- src/components/write.js
+++ src/components/write.js
@@ -13,16 +13,16 @@ class Write extends React.Component {
                 <span aria-hidden="true" className="fa fa-pencil-square-o" />
                 Write in the box:
               </label>
-              <textarea accesskey="w" id="text-box" name="text-box" wrap="soft" />
+              <textarea id="text-box" name="text-box" wrap="soft" />
             </fieldset>
             <fieldset id="box-buttons">
-              <button accesskey="l" className="read" id="read-aloud" name="read-aloud" type="submit" value="Read Aloud">
+              <button className="read" id="read-aloud" name="read-aloud" type="submit" value="Read Aloud">
                 <span aria-hidden="true" className="fa fa-bullhorn" /> Read Aloud
               </button>
-              <button accesskey="k" className="read" id="check-spelling" name="check-spelling" type="submit" value="Check Spelling">
+              <button className="read" id="check-spelling" name="check-spelling" type="submit" value="Check Spelling">
                 <span aria-hidden="true" className="fa fa-check-square-o" /> Check Spelling
               </button>
-              <button accesskey="r" className="read" id="clear" name="reset" type="reset" value="Reset">
+              <button className="read" id="clear" name="reset" type="reset" value="Reset">
                 <span aria-hidden="true" className="fa fa-trash" />
                 Reset (Delete)
               </button>
@@ -34,13 +34,13 @@ class Write extends React.Component {
           <form action="#" id="spelling-actions" method="get" name="spelling-choices">
             <ul id="spelling-choices" />
             <div id="more-container">
-              <button accesskey="m" className="spelling" id="more-words" name="more-words" type="submit" value="">
+              <button className="spelling" id="more-words" name="more-words" type="submit" value="">
                 More
                 <span aria-hidden="true" className="fa fa-arrow-circle-right" />
               </button>
             </div>
             <fieldset id="spelling-alt-actions">
-              <button accesskey="n" className="spelling spelling-button" id="ignore" name="ignore" type="submit" value="Ignore Spelling">
+              <button className="spelling spelling-button" id="ignore" name="ignore" type="submit" value="Ignore Spelling">
                 <span className="fa fa-caret-square-o-down" aria-hidden="true" /> Ignore
               </button>
             </fieldset>
