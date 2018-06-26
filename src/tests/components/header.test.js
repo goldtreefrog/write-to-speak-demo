c327982ba8f320c2c67d5476f978ace4bb5314c6
Margaret Blauvelt <margaret@blauvelt.com>
2018-06-26 05:55:57 -0700

    Fix header.test.js to use unconnected component

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
-import Header from "./../../components/header.js";
+import { Header } from "./../../components/header.js";
 
 it("renders without crashing and matches snapshot", () => {
   const wrapper = shallow(<Header writing={{ isEditing: false, editingPage: "edit", snippetsAvail: true }} />);
