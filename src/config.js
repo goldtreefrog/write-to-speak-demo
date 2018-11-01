4fd997fbb8c17a8a0f4eb04171e697372f3e2f06
Margaret Blauvelt <margaret@blauvelt.com>
2018-10-31 23:22:22 -0700

    Fix environment variables and change src/config.js to match.

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.REACT_APP_API_BASE_URL + ":" + process.env.REACT_APP_FAR_PORT;
+export const REACT_BASE_URL =
+  process.env.REACT_APP_BASE_URL + ":" + process.env.REACT_APP_PORT;
